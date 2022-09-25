235aa86ed28f8748e2e7b47de0476581e6e9f3cb
Harshul Jain <harshulgangwal02@gmail.com>
2022-09-25 10:37:00 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+
+
+
+
+
+
+app.listen(3000, function() {console.log("Server is running on port 3000")});
