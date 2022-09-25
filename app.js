07b2dcf25c64168eda58e9d947be68d5576da630
Harshul Jain <harshulgangwal02@gmail.com>
2022-09-25 15:03:21 +0530

    Added route chaining  method and added get,post,put,patch,delete method

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req,res){
+
+////////////////////////////////////////////////////////////////Request targeting all the articles
+
+app.route("/articles")
+.get(function(req,res){
     Article.find(function(err, foundArticles){
         if(!err){
         res.send(foundArticles);
@@ -26,9 +30,8 @@ app.get("/articles", function(req,res){
             res.send(err);
         }
     });
-});
-
-app.post("/articles", function(req,res)
+})
+.post(function(req,res)
 {
     const newArticle = new Article({
         title: req.body.title,
@@ -43,8 +46,76 @@ app.post("/articles", function(req,res)
             res.send(err);
         }
     });
+})
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err)
+        {
+            res.send("Successfully deleted all the articles");
+        }else{
+            res.send(err);
+        }
+    })
 });
 
+////////////////////////////////////////////////////////////////Request targeting a specific articles
+
+app.route("/articles/:articleTitle")
+
+.get(function(req,res){
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+        if(foundArticle)
+        {
+            res.send(foundArticle);
+        }else{
+            res.send("No article matching that title was found.")
+        }
+    });
+})
+
+.put(function(req, res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        function(err){
+            if(!err){
+                res.send("Successfully Updated");
+            }
+            // else{
+            //     res.send(err);
+            // }
+        }
+    );
+})
+
+.patch(function(req,res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(!err){
+                res.send("Successfully updated");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+})
+
+.delete(function(req,res){
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+            if(!err)
+            {
+                res.send("Successfully Deleted");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+});
 
 
 app.listen(3000, function() {console.log("Server is running on port 3000")});
